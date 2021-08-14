25d380c4812f45ae64d91e68abb9945fa7dba44b
MishaAkram <mishaakrambaig@gmail.com>
2021-08-15 02:28:09 +0500

    4 key input based password added

--- MBSD-assignment/MBSD-assignment/main.c
+++ MBSD-assignment/MBSD-assignment/main.c
@@ -5,26 +5,36 @@
 int GetKeyPressed(void);
 int main(void)
 {
-	char digit[20]={'1','2','3','A','4','5','6','B','7','8','9','C','*','0','#','D','\0'};
+	char digit[20]={'7','8','9','A','4','5','6','B','1','2','3','C','*','0','#','D','\0'};
 	DDRD=0xF0;
 	DDRA=0xFF;
 	DDRC=0x00;
 	DDRB=(1<<PINB1);
 	int key;
-	char password='7';
-	PORTA=digit[16];
+	char password[4]={'1','2','3','4'};
+	int index=0;
+	char open=0b11000000;
+	char close=0b11000110;
+	PORTA=0b11000110;
 	while(1)
 	{
 		//TODO:: Please write your application code
 		key= GetKeyPressed();
-		if(key!=16)
+		if(key !=16)
 		{
-			//PORTA=digit[key];
-			PORTA=0b11000000;
-			
+			if (digit[key]==password[index]){
+				index++;
+			}
+			else if(digit[key]=='0'){
+				index=0;
+				PORTA=close;
+			}
 		}
-		if(key==16){
-			PORTA=0b11000110;
+		if (index==4){
+			PORTA=open;
+		}
+		else{
+			PORTA=close;
 		}
 	}
 }
